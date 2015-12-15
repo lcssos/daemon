b92de990aad4248b1b7661146420af65b32f7f52
lcssos <lcs@hh-medic.com>
2015-12-15 18:55:54 +0800

    service打包

---
+++ 01.CommonModule/0101.System/src/main/java/cn/martin/dict/entity/Dict.java
@@ -0,0 +1,14 @@
+package cn.martin.dict.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by lcssos on 15/12/15.
+ */
+@Getter
+@Setter
+public class Dict {
+    private Integer id;
+}
+
